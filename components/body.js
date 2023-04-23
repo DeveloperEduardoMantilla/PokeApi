aa9a14f2463456419a87bc4234f1eaa446eb9e6d
EduardoMantillaCampus <mantillagarciaedgareduardo@gmail.com>
2023-04-23 13:49:25 -0500

    feat(Consumo de APis): :sparkles: Ajustes en archivo workers
    Se modifico el archivo worker con el fin de que se renderice la informacion

--- components/body.js
+++ components/body.js
@@ -1,13 +1,14 @@
 export default {
     showBody(){
-
         const worker = new Worker('./storage/wsPokemon.js')
         worker.postMessage({type: 'showPokemon'});
         worker.onmessage = function(event) {
-            //document.querySelector("#App").insertAdjacentHTML("beforeend", event.data);
-            console.log(event.data);
+            document.querySelector("#pokemones").insertAdjacentHTML("beforeend",event.data)
             worker.terminate();
+        };        
 
-        };
+        document.querySelector("#siguiente").addEventListener("click",()=>{
+            document.querySelector("#pokemones").innerHTML="<h2>Siguiente</h2>"
+        })
     }
 }
--- storage/wsPokemon.js
+++ storage/wsPokemon.js
@@ -1,20 +1,43 @@
 const llamadoApi= async()=>{
+    
     try{
         const respuesta = await fetch("https://pokeapi.co/api/v2/pokemon/?offset=0&limit=1281")
         const data = await respuesta.json()
-        
-        let resultado=[]
-        
-        for(i=0; i<data.count;i++){
-            const pokemon = await fetch("https://pokeapi.co/api/v2/pokemon/"+i)
-            const data1 = await pokemon.json()
-
-            resultado.unshift(data1.results[i])
+        let resultado="";
+        for(i=0; i<12;i++){
+            const pokemon = await fetch(data.results[i].url);
+            const data1 = await pokemon.json();
+            let plantilla = `
+                <div class="pokemon">
+                    <div class="cont">
+                        <img src="${data1.sprites.other.dream_world.front_default}" alt="">
+                    </div>
+                    <div class="info">
+                        <div class="name">
+                        <h3>${data1.name}</h3>
+                        </div>
+                        <div class="detailes">
+                        <div class="detail">
+                            <h2>Ability</h2>
+                            <h3>${data1.abilities[0].ability.name}</h3>
+                        </div>
+                        <div class="detail">
+                            <h2>Experience</h2>
+                            <h3>${data1.base_experience}</h3>
+                        </div>
+                        <div class="detail">
+                            <h2>Species</h2>
+                            <h3>${data1.species.name}</h3>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+                `;
+            resultado+=(plantilla)
         }
-
         return resultado
     }catch(e){
-        return "Error al mostrar la data";
+        return "Error al mostrar la data "+e;
     }
 }
 
