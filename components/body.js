e8add75518165bfb6cb93011a9baefd21585f214
EduardoMantillaCampus <mantillagarciaedgareduardo@gmail.com>
2023-04-27 07:24:55 -0500

    feat(Consumo de APis): :sparkles: Ajuste en el worker
    Se realizan ajustes en el worker para poder tener la funcion de buscar

--- components/body.js
+++ components/body.js
@@ -1,14 +1,36 @@
 export default {
+    
     showBody(){
+
         const worker = new Worker('./storage/wsPokemon.js')
         worker.postMessage({type: 'showPokemon'});
         worker.onmessage = function(event) {
-            document.querySelector("#pokemones").insertAdjacentHTML("beforeend",event.data)
-            worker.terminate();
-        };        
+        document.querySelector("#pokemones").insertAdjacentHTML("beforeend",event.data)
+        worker.terminate();
+        }; 
 
-        document.querySelector("#siguiente").addEventListener("click",()=>{
-            document.querySelector("#pokemones").innerHTML="<h2>Siguiente</h2>"
+        document.querySelector("#buscador").addEventListener("click",()=>{
+            let val= document.querySelector("#txt-busqueda").value;
+            const worker = new Worker('./storage/wsPokemon.js')
+            worker.postMessage({type: 'findPokemon', data:val});
+            worker.onmessage = function(e) {
+                document.querySelector("#pokemones").innerHTML= ""+e.data
+                worker.terminate();
+            };      
+            
         })
+        /* document.querySelector("#txt-busqueda").addEventListener("change",(e)=>{
+
+            if(e.target.value===""){
+                    const worker = new Worker('./storage/wsPokemon.js')
+                    worker.postMessage({type: 'showPokemon'});
+                    worker.onmessage = function(event) {
+                    document.querySelector("#pokemones").innerHTML=event.data
+                    worker.terminate();
+                    }
+    
+                
+            }
+        }) */
     }
 }
--- storage/wsPokemon.js
+++ storage/wsPokemon.js
@@ -1,10 +1,9 @@
-const llamadoApi= async()=>{
-    
+const llamadoApi= async()=>{ 
     try{
         const respuesta = await fetch("https://pokeapi.co/api/v2/pokemon/?offset=0&limit=1281")
         const data = await respuesta.json()
         let resultado="";
-        for(i=0; i<120;i++){
+        for(i=0; i<8;i++){
             const pokemon = await fetch(data.results[i].url);
             const data1 = await pokemon.json();
             let plantilla = `
@@ -41,10 +40,57 @@ const llamadoApi= async()=>{
     }
 }
 
+const buscarPokemon= async(textoBuscar)=>{ 
+    try{
+        const respuesta = await fetch("https://pokeapi.co/api/v2/pokemon/"+textoBuscar)
+        const data = await respuesta.json()
+        let plantilla = `
+                <div class="pokemon">
+                    <div class="cont">
+                        <img src="${data.sprites.other.dream_world.front_default}" alt="">
+                    </div>
+                    <div class="info">
+                        <div class="name">
+                        <h3>${data.name}</h3>
+                        </div>
+                        <div class="detailes">
+                        <div class="detail">
+                            <h2>Ability</h2>
+                            <h3>${data.abilities[0].ability.name}</h3>
+                        </div>
+                        <div class="detail">
+                            <h2>Experience</h2>
+                            <h3>${data.base_experience}</h3>
+                        </div>
+                        <div class="detail">
+                            <h2>Species</h2>
+                            <h3>${data.species.name}</h3>
+                        </div>
+                        </div>
+                    </div>
+                </div>
+                `;
+        return plantilla
+    }catch(e){
+        return "<h2>Pokemon no encontrado :( </h2>";
+    }
+}
 
 self.addEventListener('message', async function(event){
+    const data = event.data;
+    let result;
+    
+    switch(data.type){
+        case 'showPokemon':
+            result= await llamadoApi();
+            break;   
+        case 'findPokemon':
+            result= await buscarPokemon(data.data);
+            break; 
+        default:
+            result="False"
+    }
 
-    let result= await llamadoApi();
     self.postMessage(result);
 
 });
